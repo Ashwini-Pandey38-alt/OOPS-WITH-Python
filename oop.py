fcf3d1745b1f00a4b08fd66a50317a7e9ea41ba0
ashwini pandey <ashwinipandit38@gmail.com>
2026-09-08 10:21:08 +0530

    using init method

--- oop.py
+++ oop.py
@@ -1,6 +1,15 @@
-class student:
-    sub='english'
-    college='srist'
+# class student:
+#     sub='english'
+#     college='srist'
 
-stu1=student()
-stu2=student()
+# stu1=student()
+# stu2=student()
+
+'''using init methode in class and the object'''
+class employe:
+    def __init__(self,name,salary,):
+        self.name=name
+        self.salary=salary
+emply1=('ashwini',80000)
+emply2=('mohit',60000)
+print(emply1,emply2)
