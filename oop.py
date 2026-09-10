9651096376cef1271340ef9af6b9f1fac6dc36e5
ashwini pandey <ashwinipandit38@gmail.com>
2026-09-10 18:16:00 +0530

    makeing code for class and instence attribute

--- oop.py
+++ oop.py
@@ -1,32 +1,45 @@
-class student:
-    sub='english'
-    college='srist'
+# class student:
+#     sub='english'
+#     college='srist'
 
-stu1=student()
-stu2=student()
-print(stu1.sub,stu2.sub)
+# stu1=student()
+# stu2=student()
+# print(stu1.sub,stu2.sub)
 
-'''using init methode in class and the object'''
-class employe:
-    def __init__(self,name,salary,):
-        self.name=name
-        self.salary=salary
-emply1=('ashwini',80000)
-emply2=('mohit',60000)
-print(emply1,emply2)
+# '''using init methode in class and the object'''
+# class employe:
+#     def __init__(self,name,salary,):
+#         self.name=name
+#         self.salary=salary
+# emply1=('ashwini',80000)
+# emply2=('mohit',60000)
+# print(emply1,emply2)
 
-''' using instance method '''
-class student :
-    def __init__( self,name,cgpa):
-        self.name = name 
-        self.cgpa = cgpa
+# ''' using instance method '''
+# class student :
+#     def __init__( self,name,cgpa):
+#         self.name = name 
+#         self.cgpa = cgpa
 
-    def get_cgpa(self):
-        return self
+#     def get_cgpa(self):
+#         return self
 
-stu1=student('rahul',8.2)
-stu2=student('akash',8.7)
+# stu1=student('rahul',8.2)
+# stu2=student('akash',8.7)
 
-print(stu1.get_cgpa())
+# print(stu1.get_cgpa())
 
 
+
+'''class attributes and instance attributes '''
+class Vidyalay :
+    college = 'abc'
+    pi=3.1 # class attribute 
+    def __init__ (self , name , clas ) :
+        self.name =name 
+        self.clas = clas 
+        self.pi=3.14 # instance attribute 
+
+stu1= Vidyalay('ashwini',12)
+print( stu1.pi)
+print(Vidyalay.pi)
